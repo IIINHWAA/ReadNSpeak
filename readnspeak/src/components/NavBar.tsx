842bb30420fcf73509af37d8b3472dd196aaff59
INAMON <dlsghk0711@naver.com>
2024-12-18 17:43:01 +0900

    ✨feat : 내비게이션 바 UI 개발
    - 웹 사이트 로고는 추후 제작

--- readnspeak/src/components/NavBar.tsx
+++ readnspeak/src/components/NavBar.tsx
@@ -3,9 +3,10 @@ import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <nav className="bg-white text-white h-20">
+    <nav className="bg-white  h-20">
       <div className="w-full md:w-[80%] mx-auto flex justify-between items-center h-full">
-        <span className="font-bold text-black">읽고말해</span>
+        <span className="font-extrabold text-xl text-black">읽고말해</span>
+        <span>로그인</span>
       </div>
     </nav>
   );
