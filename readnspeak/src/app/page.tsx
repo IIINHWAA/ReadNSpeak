e26a6e37be6fcc9baf58a22b6b1d218e60015630
INAMON <dlsghk0711@naver.com>
2024-12-18 17:43:50 +0900

    ✨feat : 광고 배너 슬라이더 컴포넌트 정의
    - 지금은 무료 이미지로 제작
    - 추후에 변경 예정

--- readnspeak/src/app/page.tsx
+++ readnspeak/src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Banner from '@/components/Banner';
+import SearchBar from '@/components/Search';
 
 export default function Home() {
   return (
     <div>
-      
+
+      <Banner></Banner>
+      {/* <SearchBar size="small" shape="rounded" color="blue" /> */}
     </div>
   );
 }
