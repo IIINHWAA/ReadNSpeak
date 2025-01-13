c31e486b90fdb264d663ce3bba6653fdd69ee60e
INAMON <dlsghk0711@naver.com>
2025-01-13 17:26:06 +0900

    ✨feat : 도서 인터페이스 정의

---
+++ readnspeak/src/types.ts
@@ -0,0 +1,13 @@
+export interface Book {
+  book_id: number;
+  isbn: string;
+  title: string;
+  author: string;
+  publisher: string;
+  publication_date: string;
+  cover_image: string;
+  category: string;
+  language: string;
+  page_count: number;
+  created_at: string;
+}
