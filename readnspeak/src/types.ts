c5d918573d8bf7d58002a4c871591dbf12bc3886
INAMON <dlsghk0711@naver.com>
2025-01-15 16:45:26 +0900

    ✨feat : 인터페이스 설정

--- readnspeak/src/types.ts
+++ readnspeak/src/types.ts
@@ -14,3 +14,26 @@ export interface Book {
   comments : string;
   description : string;
 }
+export interface Review {
+  review_id : number;
+  user_id: number;
+  book_id: number;
+  content: string;
+  rating: number;
+  is_spoiler: boolean;
+  helpful_count: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface User {
+  user_id: number;
+  username: string;
+  email: string;
+  password_hash: string;
+  profile_picture: string;
+  bio: string;
+  last_login: string;
+  role: string;
+  created_at: string;
+}
